7a77f1f680979084ec016b493270ab7c12b198fc
jmgamer12 <jgsmartguy23@gmail.com>
2015-07-23 16:57:24 -0400

    Changed directories

--- richie-caboose/main.py
+++ richie-caboose/main.py
@@ -24,7 +24,7 @@ jinja_environment = jinja2.Environment(loader=
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render())
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
